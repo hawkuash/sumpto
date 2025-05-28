6b3f8d3c963b638a6a2a02acd293642f9feac5bf
Hawk <hawkuash@yandex.ru>
2025-05-28 14:19:39 +0300

    refactror: change module and function names

---
+++ files/files.go
@@ -0,0 +1,101 @@
+package files
+
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+	"slices"
+	"strings"
+)
+
+func deduplicate(paths []string) []string {
+	slices.Sort(paths)
+	n := len(paths)
+
+	var deduplicated []string
+	for i, j := 0, 0; i < n && j < n; {
+		if i == j {
+			deduplicated = append(deduplicated, paths[i])
+		}
+
+		if strings.Contains(paths[j], paths[i]) {
+			j += 1
+		} else {
+			i = j
+		}
+	}
+
+	return deduplicated
+}
+
+func globRec(dir string, ext []string) ([]string, error) {
+
+	files := []string{}
+	err := filepath.Walk(dir, func(path string, f os.FileInfo, err error) error {
+		if slices.Contains(ext, filepath.Ext(path)) {
+			files = append(files, path)
+		}
+		return nil
+	})
+
+	return files, err
+}
+
+func glob(dir string, ext []string) ([]string, error) {
+
+	files := []string{}
+	li, err := filepath.Glob(dir + "/*.*")
+	if err != nil {
+		return nil, err
+	}
+	for _, path := range li {
+		if slices.Contains(ext, filepath.Ext(path)) {
+			files = append(files, path)
+		}
+	}
+	return files, err
+}
+
+func ParseInput(s string) ([]string, []string) {
+	var files, paths []string
+	l := filepath.SplitList(s)
+	for _, val := range l {
+		fi, err := os.Stat(val)
+		if err != nil {
+			fmt.Println(err)
+			continue
+		}
+		if fi.IsDir() && !slices.Contains(paths, val) {
+			paths = append(paths, val)
+		}
+		if !fi.IsDir() && !slices.Contains(files, val) {
+			files = append(files, val)
+		}
+	}
+	return paths, files
+}
+
+func GenerateFiles(in string, rec bool, ext []string) []string {
+	var files, paths []string
+	paths, files = ParseInput(in)
+	if rec {
+		paths = deduplicate(paths)
+		for _, path := range paths {
+			found, err := globRec(path, ext)
+			if err == nil {
+				files = append(files, found...)
+			}
+		}
+	}
+	if !rec {
+		for _, path := range paths {
+			found, err := glob(path, ext)
+			if err == nil {
+				files = append(files, found...)
+			}
+		}
+	}
+	slices.Sort(files)
+	files = slices.Compact(files)
+	return files
+}
