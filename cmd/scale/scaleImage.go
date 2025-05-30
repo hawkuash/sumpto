51259f18fdfbf774c1629e9941db4ed04f7c72df
Hawk <hawkuash@yandex.ru>
2025-05-30 19:08:54 +0300

    feat: downscaling somewhat implemented

--- cmd/scale/scaleImage.go
+++ cmd/scale/scaleImage.go
@@ -3,8 +3,10 @@ package scale
 import (
 	"os"
 
+	"github.com/anthonynsimon/bild/imgio"
 	"github.com/davidbyttow/govips/v2/vips"
 	"github.com/hawkuash/sumpto/files"
+	"github.com/hawkuash/sumpto/presets"
 )
 
 func ScaleImage(path string, ep *vips.ExportParams, overwrite bool) {
@@ -12,10 +14,24 @@ func ScaleImage(path string, ep *vips.ExportParams, overwrite bool) {
 	if !files.CheckError(err, false) {
 		image.Resize(0.5, vips.KernelAuto)
 	}
-	imagebytes, _, err := image.ExportNative()
-	if !overwrite {
-		path = files.NewFilename(path, "downscaled")
+	switch imgtype := image.Format(); imgtype {
+	case vips.ImageTypeJPEG:
+		bytes, _, _ := image.ExportJpeg(presets.JpegExportParams(100))
+		if !overwrite {
+			path = files.NewFilename(path, "downscaled")
+		}
+		err = os.WriteFile(path, bytes, 0644)
+	case vips.ImageTypePNG:
+		newImage, _ := image.ToImage(presets.PngExportParamsSafe())
+		if !overwrite {
+			path = files.NewFilename(path, "downscaled")
+		}
+		imgio.Save(path, newImage, imgio.PNGEncoder())
 	}
-	err = os.WriteFile(path, imagebytes, 0644)
-	files.CheckError(err, false)
+	// imagebytes := setExport(image)
+	// if !overwrite {
+	// 	path = files.NewFilename(path, "downscaled")
+	// }
+	// err = os.WriteFile(path, imagebytes, 0644)
+	// files.CheckError(err, false)
 }
