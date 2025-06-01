e2d999d51fbd1b1face31b27b5dfd5b4458bc2ce
Hawk <hawkuash@yandex.ru>
2025-06-01 20:19:09 +0300

    feat: update scale command
    -add output on run
    -add flags

--- cmd/scale/scale.go
+++ cmd/scale/scale.go
@@ -3,35 +3,32 @@ package scale
 import (
 	"fmt"
 
-	"github.com/hawkuash/sumpto/cmd"
+	"github.com/hawkuash/sumpto/files"
+	"github.com/hawkuash/sumpto/scale"
 	"github.com/spf13/cobra"
 )
 
-// scaleCmd represents the scale command
-var scaleCmd = &cobra.Command{
-	Use:   "scale",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
+var search_options scale.Search_options
 
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
-	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("scale called")
+var overwrite bool
+
+// ScaleCmd represents the scale command
+var ScaleCmd = &cobra.Command{
+	Use:   "scale",
+	Short: " ",
+	Long:  `scale command`,
+	Run: func(scmd *cobra.Command, args []string) {
+		for _, file := range files.GenerateFiles(files.Paths, files.Recursive, scale.SetScaleExtensions(search_options)) {
+			fmt.Println(file)
+		}
 	},
 }
 
 func init() {
-	cmd.RootCmd.AddCommand(scaleCmd)
+	ScaleCmd.Flags().BoolVar(&search_options.Jpeg, "jpeg", false, "sets JPEG format for search")
+	ScaleCmd.Flags().BoolVar(&search_options.Png, "png", false, "sets PNG format for search")
+	ScaleCmd.Flags().BoolVar(&search_options.All, "all", false, "sets all fupported formats for search")
+	ScaleCmd.MarkFlagsOneRequired("jpeg", "png", "all")
 
-	// Here you will define your flags and configuration settings.
-
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// scaleCmd.PersistentFlags().String("foo", "", "A help for foo")
-
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// scaleCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	ScaleCmd.Flags().BoolVarP(&overwrite, "overwrite", "o", false, "declares if file should be overwritten")
 }
