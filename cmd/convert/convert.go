cb853337fe83b22c3530932908b84ea4cdbafa24
Hawk <hawkuash@yandex.ru>
2025-06-04 14:32:47 +0300

    feat:add subcommands
    - add to-png subcommand
    - add to-jpeg subcommand

--- cmd/convert/convert.go
+++ cmd/convert/convert.go
@@ -9,26 +9,14 @@ import (
 // convertCmd represents the convert command
 var ConvertCmd = &cobra.Command{
 	Use:   "convert",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "A parent command for conversion",
+	Long:  `Wait for an update, sry`,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("convert called")
+		fmt.Println("Good you called it. To convert image, call corresponding subcommand")
 	},
 }
 
 func init() {
-	// Here you will define your flags and configuration settings.
-
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// convertCmd.PersistentFlags().String("foo", "", "A help for foo")
-
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// convertCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	ConvertCmd.AddCommand(TojpegCmd)
+	ConvertCmd.AddCommand(TopngCmd)
 }
