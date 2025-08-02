af641f0a91fe6b3b15ade3cc003b938adcf4fb97
shimeoki <shimeoki@gmail.com>
2025-08-02 17:33:25 +0300

    fix: use correct naming in image.go
    also add newlines and wrap lines to 80 chars for readability

--- scale/image.go
+++ scale/image.go
@@ -12,44 +12,61 @@ import (
 )
 
 var (
-	supported_formats = []string{".jpg", ".jpeg", ".png"}
+	supportedFormats = []string{".jpg", ".jpeg", ".png"}
 )
 
-func SetScaleExtensions(format_list []string) (extensions []string) {
-	files.LowerSlice(format_list)
-	if format_list == nil {
-		return supported_formats
+func SetScaleExtensions(formatList []string) (extensions []string) {
+	files.LowerSlice(formatList)
+
+	if formatList == nil {
+		return supportedFormats
 	}
-	if slices.Contains(format_list, "jpeg") || slices.Contains(format_list, "jpg") {
+
+	if slices.Contains(formatList, "jpeg") ||
+		slices.Contains(formatList, "jpg") {
 		extensions = append(extensions, ".jpg", ".jpeg")
 	}
-	if slices.Contains(format_list, "png") {
+
+	if slices.Contains(formatList, "png") {
 		extensions = append(extensions, ".png")
 	}
+
 	return
 }
 
 func ScaleImage(path string, overwrite bool, limit int) {
 	image, err := vips.NewImageFromFile(path)
 	if err != nil {
-		log.Printf("An error occured during opening file at %s: %s \n", path, err)
+		log.Printf(
+			"An error occured during opening file at %s: %s \n",
+			path,
+			err,
+		)
 		return
 	}
+
 	if min(image.Height(), image.Width()) < limit {
 		log.Printf("Image dimensions exceeded size limit: %s", path)
 		return
 	}
+
 	image.Resize(0.5, vips.KernelAuto)
 	files.RemoveBloat(image)
+
 	switch imgtype := image.Format(); imgtype {
 	case vips.ImageTypeJPEG:
 		bytes, _, _ := image.ExportJpeg(presets.JPEG(files.Quality))
 		if !overwrite {
 			path = files.UpdateFilename(path, "downscaled")
 		}
+
 		err = os.WriteFile(path, bytes, 0644)
 		if err != nil {
-			log.Printf("An error occured during saving file at %s: %s \n", path, err)
+			log.Printf(
+				"An error occured during saving file at %s: %s \n",
+				path,
+				err,
+			)
 			return
 		}
 	case vips.ImageTypePNG:
@@ -57,9 +74,14 @@ func ScaleImage(path string, overwrite bool, limit int) {
 		if !overwrite {
 			path = files.UpdateFilename(path, "downscaled")
 		}
+
 		err = imgio.Save(path, newImage, imgio.PNGEncoder())
 		if err != nil {
-			log.Printf("An error occured during saving file at %s: %s \n", path, err)
+			log.Printf(
+				"An error occured during saving file at %s: %s \n",
+				path,
+				err,
+			)
 			return
 		}
 	default:
